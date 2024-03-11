9eda2ed3763e6b637353a1932878c7977824f6f0
Aaron Clarke <aaclarke@google.com>
2024-03-11 16:47:47 -0700

    added image

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,15 @@
+
 import 'package:flutter/material.dart';
+import 'dart:ui' as ui;
+
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(const MyHomePage());
 }
 
 // Edit function here:
-void _paint(Canvas canvas, Size size) {
+void _paint(Canvas canvas, Size size, ui.Image? image) {
   final Rect rect = Offset.zero & size;
   const RadialGradient gradient = RadialGradient(
     center: Alignment(0.7, -0.6),
@@ -17,19 +21,52 @@ void _paint(Canvas canvas, Size size) {
     rect,
     Paint()..shader = gradient.createShader(rect),
   );
+  if (image != null) {
+    canvas.drawImage(image, const Offset(10, 200), Paint());
+  }
 }
 
 class MyPainter extends CustomPainter {
+  final ui.Image? _image;
+
+  MyPainter(this._image);
+
   @override
-  void paint(Canvas canvas, Size size) => _paint(canvas, size);
+  void paint(Canvas canvas, Size size) => _paint(canvas, size, _image);
   @override
   bool shouldRepaint(MyPainter oldDelegate) => true;
   @override
   bool shouldRebuildSemantics(MyPainter oldDelegate) => false;
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyHomePageState();
+  }
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  ui.Image? _image;
+
+  Future<ui.Image> loadImageFromAssets(String path) async {
+    final ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List());
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return fi.image;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    loadImageFromAssets('assets/airplane.jpg').then((value) {
+      setState(() {
+        _image = value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -37,7 +74,7 @@ class MyHomePage extends StatelessWidget {
         builder: (BuildContext context, BoxConstraints constraints) {
       return CustomPaint(
         size: Size(constraints.maxWidth, constraints.maxHeight),
-        painter: MyPainter(),
+        painter: MyPainter(_image),
       );
     });
   }
